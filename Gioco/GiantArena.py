ce213968ab8e8c3e26f0cb0e5111c18475df6aa6
Francesco <ridolfinotv@gmail.com>
2018-06-03 22:42:43 +0200

    Update GiantArena.py

--- Gioco/GiantArena.py
+++ Gioco/GiantArena.py
@@ -1,13 +1,14 @@
 import pygame, time, socket, ast
 from random import randint
+from Libs.Connection import Conn
+from Libs.GUI import GUI
+
 
 pygame.init()
 
-
-frasi = ["Dio Sasso","Dio Mattone","Ti denuncio","Osanna eh", "Lo faccio per voi"]
-
 img_path = "Images/"
 font_path = "Fonts/"
+
 
 def getIMG(name):
     img = pygame.image.load(img_path+name)
@@ -17,115 +18,92 @@ def getFont(name,size=10):
     f = pygame.font.Font(font_path+name,size)
     return f
 
-def getMillis():
-    return round(time.time()*1000)
 
-font = getFont("Avenir3.otf",30)
 
 host = input("Inserire l'host del server: ")
 port = input("Inserire la porta del server(9999 Default): ")
 port = (int(port) if port != "" else 9999)
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host,port))
-
-
 nick = input("Perfavore scegli un nickname: ")
 
-ciccio = getIMG("player.png")
-x=100
-y=100
-speed = 2
+conn = Conn(host,port)
+conn.startConn()
 
-xt = x
-yt = y
+fonts = [getFont("Avenir3.otf",30),getFont("alternate gothic no2.ttf",20)]
+avatar = getIMG("player.png")
+speed = 5
 
-s_size = (600,400)
+
+s_size = (1080,820)
 s_bgcolor = (87, 88, 89)
 
 display = pygame.display
 
 screen = display.set_mode(s_size)
-display.set_caption("GiantArena")
+display.set_caption("CiccioGame89")
 display.flip()
 
-clicks = 0
-start = 0
+gui = GUI(screen,display,fonts,avatar,nick,conn)
+
+x = 100
+y = 100
 
 Process = True
-pp = False
+writing = False
+
+text = "Cliccami per scrivere!"
 
 while Process:
-    txt = ""
-    xt = x
-    yt = y
     screen.fill(s_bgcolor)
-    nickname = font.render(nick[:9], True, (255,255,255))
-    screen.blit(nickname,(x,y-30))
-    screen.blit(pygame.transform.scale(ciccio, (100,100)),(x,y))
+    msg = fonts[1].render(text, True, (255,255,255))
+    screen.blit(msg,(695,185))
+    elements = gui.runDisplay()
+    xy = elements[0]
+    box = elements[1]
+    x = xy[0]
+    y = xy[1]
     for event in pygame.event.get():
         if(event.type == pygame.QUIT):
             Process = False
             pygame.quit()
+            print("Game ended at: "+time.ctime())
             exit()
+        if(event.type == pygame.MOUSEBUTTONDOWN):
+            if(box.collidepoint(event.pos)):
+                text = ""
+                writing = True
         if(event.type == pygame.KEYDOWN):
-            if(event.key == 107):
-                txt = frasi[randint(0,len(frasi)-1)]
-                text = font.render(txt, True, (0,255,0))
-                if(y>=280):
-                    yt = y - 40
-                """if(y<=30):
-                    yt = y + 100"""
-                if(x>=500):
-                    xt = x - 30
-                screen.blit(text,(xt,yt))
-                pp = True
+            if writing:
+                if(event.key == pygame.K_RETURN):
+                    gui.sendMessage(text)
+                    text = "Cliccami per scrivere!"
+                    writing = False
+                elif(event.key == pygame.K_BACKSPACE):
+                    text = text[:-1]
+                else:
+                    text += event.unicode
+            else:
+                pass
     try:
-        keys = pygame.key.get_pressed()
-        if(keys[pygame.K_UP]):
-            y=y-speed
-        if(keys[pygame.K_DOWN]):
-            y=y+speed
-        if(keys[pygame.K_RIGHT]):
-            x=x+speed
-        if(keys[pygame.K_LEFT]):
-            x=x-speed
-        if(x<0):
-            x=0
-        if(x>500):
-            x=500
-        if(y<30):
-            y=30
-        if(y>306):
-            y=306
+        if not writing:
+            keys = pygame.key.get_pressed()
+            if(keys[pygame.K_UP]):
+                y=y-speed
+            if(keys[pygame.K_DOWN]):
+                y=y+speed
+            if(keys[pygame.K_RIGHT]):
+                x=x+speed
+            if(keys[pygame.K_LEFT]):
+                x=x-speed
+            if(x<0):
+                x=0
+            if(x>960):
+                x=960
+            if(y<30):
+                y=30
+            if(y>720):
+                y=720
     except:
-        print("Chiusura")
+        pass
     #print("x: %s, y: %s" % (x,y))
-    info = {"player":nick,"properties":{"x":x,"y":y,"frase":("None" if pp == False else txt)}}
-    s.send(str(info).encode())
-    data = ast.literal_eval(s.recv(1024).decode())
-    for k in data:
-        if(k != nick):
-            x_p = data[k]["x"]
-            y_p = data[k]["y"]
-            xt_p = x_p
-            yt_p = y_p
-            n = font.render(k, True, (255,255,255))
-            screen.blit(n,(x_p,y_p-30))
-            screen.blit(pygame.transform.scale(ciccio, (100,100)), (x_p,y_p))
-            if(data[k]["frase"] != "None"):
-                text = font.render(data[k]["frase"], True, (0,255,0))
-                if(y>=280):
-                    yt_p = y_p - 40
-                """if(y<=30):
-                    yt = y + 100"""
-                if(x>=500):
-                    xt_p = x_p - 30
-                screen.blit(text,(xt_p,yt_p))
-                display.update()
-                time.sleep(0.1)
-    display.update()
-    if(pp == True):
-        time.sleep(0.1)
-        pp = False
-print("Game ended at: "+time.ctime())
+    gui.setPos(x,y)
+
